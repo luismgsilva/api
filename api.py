bbdb3ec400d7df43515e5cf3f91d8e2ba6b23879
Luis Silva <106989662+luismgsilva@users.noreply.github.com>
2023-08-09 20:12:27 +0100

    MQTT Integration (#1)
    * MQTT Integration
    
    Implemented a handler for events from SQLAlchemy.
    Implemented an MQTT handler that detects new data
    in the database and sends a signal to another instance.
    
    Signed-off-by: Luis Silva <luis.m.silva99@hotmail.com>

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
+import paho.mqtt.client as mqtt
 
 # Define the database connection
 DATABASE_URL = "sqlite:///mydatabase.db"
@@ -68,6 +69,9 @@ def create_webhook(payload: WebhookPayload):
         
         # Write payload to file for debug purposes
         debug(payload)
+
+        # Publishes a message to an MQTT broker
+        publish_to_mqtt("webhook_data", str(payload.json()))
         
         return db_payload
         
@@ -93,10 +97,34 @@ def read_data(skip: int = 0, limit: int = 10):
 async def root():
     return {"message": "Hello World"}
 
+# from sqlalchemy import event
+# @event.listens_for(WebhookPayloadCreate, "before_insert")
+# def before_insert_listener(mapper, connection, target):
+#     print("funciona before insert: ", target.id)
+#     publish_to_mqtt("webhook_data", target.repository_name)
+
+# session = SessionsLocal()
+# @event.listens_for(session, "after_commit")
+# def after_commit_listener(session):
+#     print("funciona after commit.")
+
+# Publishes a message to an MQTT broker
+def publish_to_mqtt(topic, payload):
+    mqtt_broker = "localhost"
+    mqtt_port = 1883
+    mqtt_client = mqtt.Client()
+
+    try:
+        mqtt_client.connect(mqtt_broker, mqtt_port)
+        mqtt_client.publish(topic, payload)
+        mqtt_client.disconnect()
+    except Exception as e:
+        print("Error publishing to MQTT:", str(e))
+
 # Write payload to file for debug purposes
 import json
 def debug(payload: WebhookPayload):
-    file_path = "/home/luiss/webhook.json"
+    file_path = "webhook.json"
     with open(file_path, 'w') as json_file:
         json.dump(payload.dict(), json_file, indent=4)
         
